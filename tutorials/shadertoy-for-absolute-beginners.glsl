d686fb2d7c04997d59a0921ef926471fcdfc78f1
Pierrick Lyons <pierrick@live.co.uk>
2023-11-09 15:45:34 +0000

    part 2

--- tutorials/shadertoy-for-absolute-beginners.glsl
+++ tutorials/shadertoy-for-absolute-beginners.glsl
@@ -1,16 +1,31 @@
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
-{
+float Circle(vec2 uv, vec2 position, float radius, float blur) {
+    float distance = length(uv - position);
+    float c = smoothstep(radius, radius - blur, distance);
+    
+    return c;
+}
+
+
+void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     vec2 uv = fragCoord.xy / iResolution.xy; //Normalize vector  0 <> 1
     
     uv -= 0.5;
     uv.x *= iResolution.x/iResolution.y;
     
-    float d = length(uv);
-    float r = 0.4;
+    vec3 color = vec3(0.0, 0.0,0.0);
     
-    float c = smoothstep(r, r - 0.1, d);
+    float mask = Circle(uv, vec2(0.0, 0.0), 0.4, 0.05);
     
-    //if (d < 0.4) c = 1.0; else c = 0.0;
+    mask -= Circle(uv, vec2(-0.125, 0.1), 0.075, 0.025);
+    mask -= Circle(uv, vec2(0.125, 0.1), 0.075, 0.025);
     
-    fragColor = vec4(vec3(c), 1.0);
+    float mouth = Circle(uv, vec2(0.0, 0.0), 0.3, 0.025);
+    mouth -= Circle(uv, vec2(0.0, 0.1), 0.3, 0.025);
+    
+    
+    mask -= mouth;
+    color = vec3(1.0, 1.0, 0.0) * mask;
+    //color = vec3(mouth);
+    
+    fragColor = vec4(vec3(color), 1.0);
 }
